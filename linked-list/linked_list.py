90e7e1ac3e30c1b49d27b6a6fcf81a0cb608a9f2
Coorjet <kevin.lof@externe.francemm.com>
2019-12-02 13:59:41 +0100

    LinkedList : WIP

---
+++ linked-list/linked_list.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, value, succeeding=None, previous=None):
+        self.value = value
+        self.previous = previous
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.firstElement = null
+        self.lastElement = null
+
+    def push(self, value):
+        pass
+
+    def pop(self):
+        pass
+
+    def shift(self):
+        pass
+        
+    def unshift(self, value):
+        pass
