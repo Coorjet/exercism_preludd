e8eca8aada81601868ad03f4536ef307d45b5b8f
Coorjet <kevin.lof@externe.francemm.com>
2019-12-02 17:03:09 +0100

    Linked List OK :D

--- linked-list/linked_list.py
+++ linked-list/linked_list.py
@@ -1,23 +1,51 @@
 class Node:
     def __init__(self, value, succeeding=None, previous=None):
+
         self.value = value
         self.previous = previous
-        self.next = next
+        self.succeeding = succeeding
 
 
 class LinkedList:
     def __init__(self):
-        self.firstNode = None
-        self.lastNode = None
+        self.first = None
+        self.last = None
 
     def push(self, value):
-        pass
+        newNode = Node(value, None, self.last)
 
+        if self.last:
+            self.last.succeeding = newNode
+        self.last = newNode
+
+        if not self.first:
+            self.first = self.last
+       
     def pop(self):
-        pass
+
+       poppedVal = self.last.value
+       self.last =  self.last.previous
+       if self.last:
+           self.last.succeeding = None
+       else:
+           self.first = None
+       return poppedVal
 
     def shift(self):
-        pass
+        shiftedVal = self.first.value
+        self.first = self.first.succeeding
+        if self.first:
+            self.first.previous = None
+        else:
+            self.last = None
+        return shiftedVal
+
 
     def unshift(self, value):
-        pass
+
+        newNode = Node(value, self.first, None)
+        if self.first:
+            self.first.previous = newNode
+        self.first = newNode
+        if not self.last:
+            self.last = self.first
